268fed5219e1242d5abccaefe5dc9fc967fa78c8
Adithya0526 <sadithya.s256@gmail.com>
2026-06-30 11:25:05 +0530

    Completed Step 2 - Auth with the code above

--- request_api.py
+++ request_api.py
@@ -1,13 +1,13 @@
 from flask import jsonify
 import requests
-url=r"http://4.224.186.213/evaluation-service/register"
+url=r"http://4.224.186.213/evaluation-service/auth"
 data={
     "email":"sadithya.s256@gmail.com",
     "name":"Adithya S",
-    "mobileNo":"6383973794",
-    "githubUsername":"Adithya0526",
     "rollNo":"23CSA05",
-    "accessCode":"WjNyCT"
+    "accessCode":"WjNyCT",
+    "clientID":"62a08e87-61f2-4a68-9a84-1e942c6e702d",
+    "clientSecret":"SAbbcBnuXtneQGqH"
 }
 response=requests.post(url,json=data)
 print(response.status_code)
